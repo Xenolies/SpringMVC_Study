b4e790cffb26dd07849874e179cc4fc22fc54b05
Xenolies <62866031+Xenolies@users.noreply.github.com>
2023-03-16 10:19:43 +0800

    Create RegisterController.java and Update LoginController.java

--- src/main/java/hb/rj/controller/RegisterController.java
+++ src/main/java/hb/rj/controller/RegisterController.java
@@ -2,11 +2,15 @@ package hb.rj.controller;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
-import javax.servlet.http.*;
-public class LoginController implements Controller {
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegisterController implements Controller {
+
+    // 打开注册页前的代码
     @Override
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
-        return new ModelAndView("/login");
+        return new ModelAndView("/register");
     }
 }
