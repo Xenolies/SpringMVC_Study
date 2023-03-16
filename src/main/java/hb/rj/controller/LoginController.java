3f779977c3e84d2dd959f5f93322f6ac5fd519db
Xenolies <62866031+Xenolies@users.noreply.github.com>
2023-03-16 10:15:04 +0800

    Rename LoginController.java

--- src/main/java/hb/rj/controller/LoginController.java
+++ src/main/java/hb/rj/controller/LoginController.java
@@ -3,7 +3,7 @@ package hb.rj.controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 import javax.servlet.http.*;
-public class LoginContrller implements Controller {
+public class LoginController implements Controller {
 
     @Override
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
