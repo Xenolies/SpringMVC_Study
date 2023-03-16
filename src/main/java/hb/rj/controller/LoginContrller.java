56aecddd5b3412ad25a25c7ee7926988fe239495
Xenolies <62866031+Xenolies@users.noreply.github.com>
2023-03-16 10:05:16 +0800

    Update LoginController.java

--- src/main/java/hb/rj/controller/LoginContrller.java
+++ src/main/java/hb/rj/controller/LoginContrller.java
@@ -2,11 +2,11 @@ package hb.rj.controller;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
-
+import javax.servlet.http.*;
 public class LoginContrller implements Controller {
 
     @Override
-    public ModelAndView handleRequest(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse) throws Exception {
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
         return null;
     }
 }
